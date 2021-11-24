f76fdc1873a4c1045d60bf5f01c722bda73b8510
Arkadip <in2arkadipb13@gmail.com>
2021-11-24 18:03:03 +0530

    refactor: gatsby config - added a small description

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     description: `A Computer science and Engineering student, 
     Deep Learning and Computer Vision 
     engineer and a web developer`,
+    descriptionSmall: `Developer`,
     siteUrl: 'https://v2.arkadip.me',
     image: '/images/icon.png',
     author: `@darkmatter18`,
