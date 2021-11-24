81dc0972caaa74443fd33ec2e528df886b601ef6
Arkadip <in2arkadipb13@gmail.com>
2021-11-24 13:19:58 +0530

    feat: integrated the layout in the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import * as React from 'react';
+import Layout from '../components/layout';
+import PropTypes from 'prop-types';
 
 // markup
-const IndexPage = () => {
+const IndexPage = ({location}) => {
   return (
-    <main>
-      Hello World
-    </main>
+    <Layout location={location}>
+      <main>
+        Hello World
+      </main>
+    </Layout>
   );
+};
+
+IndexPage.propTypes = {
+  location: PropTypes.object.isRequired,
 };
 
 export default IndexPage;
