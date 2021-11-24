bac65de244ad7ba13992af01d3236426d10de221
Arkadip <in2arkadipb13@gmail.com>
2021-11-24 12:49:27 +0530

    feat: added all initial config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,25 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'neon-blue': '#3B27BA',
+        'neon-blue-2': '#21006F',
+        'neon-light-blue': '#450EFF',
+        'neon-sky': '#027A9F',
+        'neon-green': '#13CA91',
+        'neon-violet': '#E100F5',
+        'deep-red': '#330408',
+        'deep-blue': '#111935',
+
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--gradient-color-stops))',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
