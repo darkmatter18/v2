1f20eae84708e19015c9882698f305ccd0bb0935
Arkadip <in2arkadipb13@gmail.com>
2021-12-13 20:47:58 +0530

    feat: added a nav component

---
+++ src/components/sections/nav.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div className="w-full text-neon-violet z-11 fixed">
+      {/* x-data="{ open: false }"*/}
+      <div
+        className="flex flex-col max-w-screen-xl px-4 mx-auto
+        md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+        <div className="p-4 flex flex-row items-center justify-end">
+          <button
+            className="md:hidden rounded-lg focus:outline-none
+            focus:shadow-outline"
+          >
+            <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6">
+              <path fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1
+                1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0
+                011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                clipRule="evenodd"/>
+              {/* <path x-show="open" fillRule="evenodd" d="M4.293 4.293a1
+               1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414
+                10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1
+                 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                 clip-rule="evenodd"/>*/}
+            </svg>
+          </button>
+        </div>
+        {/*  :class="{'flex': open, 'hidden': !open}"*/}
+        <nav className="flex-col flex-grow pt-4 hidden
+         md:flex md:justify-end md:flex-row">
+          <a
+            className="px-4 py-2 mt-2 text-sm font-semibold
+             text-gray-900 bg-gray-200 rounded-lg dark-mode:bg-gray-700
+             dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600
+             dark-mode:focus:text-white dark-mode:hover:text-white
+             dark-mode:text-gray-200 md:mt-0 hover:text-gray-900
+             focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200
+             focus:outline-none focus:shadow-outline"
+            href="#">Blog</a>
+          <a
+            className="px-4 py-2 mt-2 text-sm font-semibold
+            bg-transparent rounded-lg dark-mode:bg-transparent
+            dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600
+            dark-mode:focus:text-white dark-mode:hover:text-white
+            dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900
+            focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200
+            focus:outline-none focus:shadow-outline"
+            href="#">Portfolio</a>
+          <a
+            className="px-4 py-2 mt-2 text-sm font-semibold
+            bg-transparent rounded-lg dark-mode:bg-transparent
+            dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600
+            dark-mode:focus:text-white dark-mode:hover:text-white
+            dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900
+            focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200
+            focus:outline-none focus:shadow-outline"
+            href="#">About</a>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
