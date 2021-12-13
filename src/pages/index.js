ec702bf73b3f81f5cd1297e994358ef6594626c2
Arkadip <in2arkadipb13@gmail.com>
2021-12-13 19:22:23 +0530

    fix: move the hero to hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,14 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import Hero from '../components/sections/hero';
 
 // markup
 const IndexPage = ({location}) => {
   return (
     <Layout location={location}>
       <SEO title="Home"/>
-      <div className={'h-screen ' +
-            'flex flex-col justify-center pl-5 sm:pl-20'}>
-        <h1 className={'text-2xl sm:text-5xl text-neon-violet-light' +
-          ' font-light font-nunito'}>Hello World</h1>
-        <h2 className={'text-4xl sm:text-7xl font-normal font-nunito'}>
-          <span className={'text-neon-violet overflow-clip'}>
-                Hi, I&apos;m Arkadip
-          </span>
-        </h2>
-        <p className={'sm:text-2xl text-neon-violet font-plex'}>
-              I&apos;m a software engineer based in India.
-        </p>
-      </div>
+      <Hero/>
     </Layout>
   );
 };
