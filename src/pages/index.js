766a77f9d1669dec1e610b44e0ceaa1d79530c56
Arkadip <in2arkadipb13@gmail.com>
2021-12-13 17:42:38 +0530

    fix: text color and flex css

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,16 @@ const IndexPage = ({location}) => {
   return (
     <Layout location={location}>
       <SEO title="Arkadip"/>
-      <div className={'h-screen text-neon-violet' +
+      <div className={'h-screen ' +
             'flex flex-col justify-center pl-20 overflow-x-visible z-10'}>
-        <h1 className={'text-5xl font-light font-nunito'}>Hello World</h1>
+        <h1 className={'text-5xl text-neon-violet' +
+          ' font-light font-nunito'}>Hello World</h1>
         <h2 className={'text-7xl font-normal font-nunito'}>
           <span className={'text-neon-violet overflow-clip'}>
                 Hi, I&apos;m Arkadip
           </span>
         </h2>
-        <p className={'text-2xl font-plex'}>
+        <p className={'text-2xl text-neon-violet font-plex'}>
               I&apos;m a software engineer based in India.
         </p>
       </div>
