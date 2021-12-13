bdde6cc149c441308bcd56f14a78c7ea67b96bb1
Arkadip <in2arkadipb13@gmail.com>
2021-12-13 18:19:06 +0530

    fix: added media query in text

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,15 @@ const IndexPage = ({location}) => {
     <Layout location={location}>
       <SEO title="Arkadip"/>
       <div className={'h-screen ' +
-            'flex flex-col justify-center pl-20 overflow-x-visible z-10'}>
-        <h1 className={'text-5xl text-neon-violet' +
+            'flex flex-col justify-center pl-5 sm:pl-20'}>
+        <h1 className={'text-2xl sm:text-5xl text-neon-violet-light' +
           ' font-light font-nunito'}>Hello World</h1>
-        <h2 className={'text-7xl font-normal font-nunito'}>
+        <h2 className={'text-4xl sm:text-7xl font-normal font-nunito'}>
           <span className={'text-neon-violet overflow-clip'}>
                 Hi, I&apos;m Arkadip
           </span>
         </h2>
-        <p className={'text-2xl text-neon-violet font-plex'}>
+        <p className={'sm:text-2xl text-neon-violet font-plex'}>
               I&apos;m a software engineer based in India.
         </p>
       </div>
