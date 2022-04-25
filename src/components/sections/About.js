5e7754624381da7ad7933db2f8a0c193593812fa
Arkadip <in2arkadipb13@gmail.com>
2022-04-25 21:47:49 +0530

    feat: added parallex in hero page

---
+++ src/components/sections/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div id={'about'} className={'h-screen pt-20 text-neon-violet'}>
+      About screen is under construction
+    </div>
+  );
+};
+
+export default About;
