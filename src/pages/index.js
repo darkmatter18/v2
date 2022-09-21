615cc72e1dd3956ed1b6faaf05e4eaf612878279
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2022-09-21 04:21:32 +0000

    fix: window not defined error

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = ({location}) => {
   return (
     <Layout location={location}>
       <SEO title="Home"/>
-      <Nav location={location} initialNavState={window.innerWidth > 768}/>
+      <Nav location={location} initialNavState={typeof window !== "undefined" && window.innerWidth > 768}/>
       <Hero/>
       <About/>
     </Layout>
