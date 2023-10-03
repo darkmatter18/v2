a223e6cfdf881f034217f16a7d9964f1741d4f7f
Arkadip Bhattacharya <hi@arkadip.dev>
2023-10-03 07:10:44 +0000

    feat: added the feature of outbound link

--- src/components/utils/navLink.js
+++ src/components/utils/navLink.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {OutboundLink} from 'gatsby-plugin-google-gtag';
+
 
 const NavLink = ({index, name, url}) => {
   console.log(index, name, url);
-  return (
-    <a
-      className="px-4 py-2 mt-2 md:mt-0 text-sm font-semibold md:ml-4
-      text-neon-violet no-underline underline-offset-1 hover:underline"
-      href={url}
-    >
-      {`${index}. ${name}`}
-    </a>
-  );
+  if (url.startsWith('http')) {
+    return (
+      <OutboundLink
+        className="px-4 py-2 mt-2 md:mt-0 text-sm font-semibold md:ml-4
+        text-neon-violet no-underline underline-offset-1 hover:underline"
+        href={url}
+        target='_blank'
+      >
+        {`${index}. ${name}`}
+      </OutboundLink>
+    );
+  } else {
+    return (
+      <a
+        className="px-4 py-2 mt-2 md:mt-0 text-sm font-semibold md:ml-4
+        text-neon-violet no-underline underline-offset-1 hover:underline"
+        href={url}
+      >
+        {`${index}. ${name}`}
+      </a>
+    );
+  }
 };
 
 NavLink.propTypes = {
